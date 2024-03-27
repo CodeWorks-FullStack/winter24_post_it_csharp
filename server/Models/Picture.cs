a2d68e3676bdf3b8f06fc89fd1fc22c7bc7193f1
Jeremy Fowler <jembyfowler@gmail.com>
2024-03-27 12:10:14 -0600

    inheriting on picture

--- server/Models/Picture.cs
+++ server/Models/Picture.cs
@@ -1,10 +1,10 @@
 namespace post_it_csharp.Models;
 
-public class Picture
+public class Picture : RepoItem
 {
-  public int Id { get; set; }
-  public DateTime CreatedAt { get; set; }
-  public DateTime UpdatedAt { get; set; }
+  // public int Id { get; set; }
+  // public DateTime CreatedAt { get; set; }
+  // public DateTime UpdatedAt { get; set; }
   public string ImgUrl { get; set; }
   public int AlbumId { get; set; }
   public string CreatorId { get; set; }
